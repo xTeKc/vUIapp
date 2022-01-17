aa744a5c98cbfade7903d64b79e842b321f05944
xTeKc <bcd3v@pm.me>
2022-01-16 22:35:20 -0500

    create resizable window

--- vUIapp.v
+++ vUIapp.v
@@ -1,5 +1,36 @@
-module main
+import ui
+import gx
+
+const (
+	win_width  = 250
+	win_height = 250
+)
+
+struct App {
+mut:
+	window &ui.Window = 0
+}
 
 fn main() {
-	println('Hello World!')
+	mut app := &App{}
+	window := ui.window(
+		width: win_width
+		height: win_height
+		title: 'Resizable Window'
+		resizable: true
+		state: app
+		children: [
+			ui.row(
+				margin_: .3
+				widths: .4
+				heights: .4
+				bg_color: gx.rgba(255, 0, 0, 20)
+				children: [
+					ui.button(text: 'Add'),
+				]
+			),
+		]
+	)
+	app.window = window
+	ui.run(window)
 }
